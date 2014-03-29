26285834108c09300320d6b78a110af68a0ad81b
ohall <ohall@grassroots-tech.com>
2014-03-29 16:13:52 -0400

    update shell script

--- deployCliner.sh
+++ deployCliner.sh
@@ -2,6 +2,13 @@
 set -u
 set -e
 
-FTP_SITE='tiller.dreamhost.com'
+
+##
+# Add FTP login info locally. DO NOT COMMIT THIS FILE WITH LOGIN INFO
+##
+
+FTP_SITE=''
+USER=''
+PASSWD=''
 echo "connecting to FTP_SITE..."
-ncftpput -R $FTP_SITE /mycliner.com/app/ /Users/oakley/Documents/projects/Podcast/myCliner/app/* #*.jpg *.xml *.mp3 *.pdf
+ncftpput -R -u $USER -p $PASSWD $FTP_SITE /myCliner.com/app/ /Users/oakley/Documents/projects/Podcast/myCliner/app/* #*.jpg *.xml *.mp3 *.pdf
